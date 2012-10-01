15103e67fa61fd456b20b810dd407f3d2dcff3c8
Chris Stevens <chris.stevens@opentaxi.com>
2012-10-01 11:18:39 -0500

    Added StatsD.php class from Etsy's php example. Updated README to include our new options and CakePHP integration examples

--- README.md
+++ README.md
@@ -1,4 +1,67 @@
-statsd-php
-==========
+PHP Class for StatsD 
+====================
 
-Sends statistics to the stats daemon over UDP
+Send statistics to the stats daemon over UDP.
+
+This class generally follows the official php-example from Etsy, but moves the
+configuration options into the main class with reasonable defaults.  This should
+make incorporation into framework-based applications more straightforward.
+
+Upstream:
+https://github.com/etsy/statsd/blob/master/examples/php-example.php
+
+License: MIT
+
+
+Options
+-------
+
+* `enabled` - boolean - Set to false to disable UDP transmission (default: true)
+
+* `prefix` - string - Apply a global namespace to all metrics from this application
+
+* `host` - string - Hostname or IP of your carbon/graphite server
+
+* `port` - integer - StatsD port on your carbon/graphite server
+
+
+CakePHP v2 Installation
+-----------------------
+
+This class is generic enough that we can use it directly with CakePHP as as library.  The
+installation instructions below assume that we want StatsD capability in all of
+our controllers, but it could applied more precisely to only the required controllers,
+models, or views if desired.
+
+1) Copy the StatsD.php class into app/Lib
+
+2) Make it available to all controllers by adding to the top of app/Controllers/AppController.php:
+App::uses('StatsD', 'Lib');
+
+2) (Optional) Update app/Config/bootstrap.php if you want to use any of the configuration options:
+Configure::write('StatsD', array(
+	'enabled' => true,
+	'prefix' => 'your-app-name',
+	'host' => 'your-carbon-server',
+	'port' => XXXX
+));
+
+3) (Optional) Add the following line to your beforeFilter in app/Controllers/AppController.php:
+StatsD::config(Configure::read('StatsD'));
+
+
+CakePHP v2 Usage
+----------------
+
+Class methods are called statically.  Use the prefix option to keep your code clean and 
+contain all of the metrics for the app within the same "bucket" in Graphite.
+
+Example: Counting login actions (successful vs failed) in a UsersController:
+`
+if (!$this->Auth->login()) {
+  StatsD::increment("logins.failed");
+  $this->Session->setFlash(__('Login failed.  Please try again'));
+  return;
+}
+StatsD::increment("logins.ok");
+`
---
+++ StatsD.php
@@ -0,0 +1,152 @@
+<?php
+/**
+ * Sends statistics to the stats daemon over UDP
+ *
+ * This class generally follows the official php-example from Etsy, but moves
+ * the configuration options into the main class.
+ *
+ * @link https://github.com/etsy/statsd/blob/master/examples/php-example.php
+ **/
+class StatsD {
+    /**
+     * Sending Enabled Flag
+     *
+     * @var boolean
+     */
+    public static $enabled = true;
+
+    /**
+     * Default prefix (e.g. myapp)
+     *
+     * In graphite, this would show up as stats.myapp.
+     *
+     * @var string
+     */
+    public static $prefix = null;
+
+    /**
+     * Default Host
+     *
+     * @var string
+     */
+    public static $host = 'localhost';
+
+    /**
+     * Default Port
+     *
+     * @var integer
+     */
+    public static $port = 8125;
+
+
+
+    /**
+     * Set configuration values
+     *
+     **/
+    public static function config($params = array()) {
+        extract($params);
+
+        if (!empty($host)) {
+            StatsD::$host = $host;
+        }
+        if (!empty($port)) {
+            StatsD::$port = $port;
+        }
+        if (!empty($prefix)) {
+            StatsD::$prefix = $prefix;
+        }
+        if (isset($enabled)) {
+            StatsD::$enabled = $enabled;
+        }
+    }
+
+    /**
+     * Log timing information
+     *
+     * @param string $stats The metric to in log timing info for.
+     * @param float $time The elapsed time (ms) to log
+     * @param float|1 $sampleRate the rate (0-1) for sampling.
+     **/
+    public static function timing($stat, $time, $sampleRate=1) {
+        StatsD::send(array($stat => "$time|ms"), $sampleRate);
+    }
+
+    /**
+     * Increments one or more stats counters
+     *
+     * @param string|array $stats The metric(s) to increment.
+     * @param float|1 $sampleRate the rate (0-1) for sampling.
+     * @return boolean
+     **/
+    public static function increment($stats, $sampleRate=1) {
+        StatsD::updateStats($stats, 1, $sampleRate);
+    }
+
+    /**
+     * Decrements one or more stats counters.
+     *
+     * @param string|array $stats The metric(s) to decrement.
+     * @param float|1 $sampleRate the rate (0-1) for sampling.
+     * @return boolean
+     **/
+    public static function decrement($stats, $sampleRate=1) {
+        StatsD::updateStats($stats, -1, $sampleRate);
+    }
+
+    /**
+     * Updates one or more stats counters by arbitrary amounts.
+     *
+     * @param string|array $stats The metric(s) to update. Should be either a string or array of metrics.
+     * @param int|1 $delta The amount to increment/decrement each metric by.
+     * @param float|1 $sampleRate the rate (0-1) for sampling.
+     * @return boolean
+     **/
+    public static function updateStats($stats, $delta=1, $sampleRate=1) {
+        if (!is_array($stats)) { $stats = array($stats); }
+        $data = array();
+        foreach($stats as $stat) {
+            $data[$stat] = "$delta|c";
+        }
+
+        StatsD::send($data, $sampleRate);
+    }
+
+    /*
+     * Squirt the metrics over UDP
+     **/
+    public static function send($data, $sampleRate=1) {
+        // check that sending is actually enabled
+        if (self::$enabled === false) { return; }
+
+        // sampling
+        $sampledData = array();
+
+        if ($sampleRate < 1) {
+            foreach ($data as $stat => $value) {
+                if ((mt_rand() / mt_getrandmax()) <= $sampleRate) {
+                    $sampledData[$stat] = "$value|@$sampleRate";
+                }
+            }
+        } else {
+            $sampledData = $data;
+        }
+
+        if (empty($sampledData)) { return; }
+
+        // Wrap this in a try/catch - failures in any of this should be silently ignored
+        try {
+            $host = self::$host;
+            $fp = fsockopen("udp://$host", self::$port, $errno, $errstr);
+            if (! $fp) { return; }
+            foreach ($sampledData as $stat => $value) {
+                if (!empty(self::$prefix)) {
+                    $stat = self::$prefix.'.'.$stat;
+                }
+                fwrite($fp, "$stat:$value");
+            }
+            fclose($fp);
+        } catch (Exception $e) {
+        }
+    }
+}
